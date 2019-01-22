283257b41b5515a4a395a1f23a0f2da02daee388
MaChFeSt <45597172+MaChFeSt@users.noreply.github.com>
2019-01-22 11:58:58 +0100

    Add files via upload

--- src/Logik/SpielLogikAI.java
+++ src/Logik/SpielLogikAI.java
@@ -1,72 +1,16 @@
 package Logik;
-import Ai.*;
 import gui.*;
-
-import javax.swing.*;
 
 public class SpielLogikAI {
 
     static boolean meinZug = true;
 
-    boolean spielLaeuft = true;
-
     static Object[][] aigrid = gui.Window.getaigrid();
 
     static Object[][] mygrid = gui.Window.getmyGrid();
 
-    int boardsize = gui.Window.getBoardsize();
-
-    int altePos [] = new int[2];
-
     /**
-     * "Spielloop" wird gestartet, das Spiel laeuft, solang die Variable
-     * spielLaeuft auf true gesetzt ist.
-     */
-    public void spielStart(){
-        aishot shot = new aishot();
-        altePos[0] = -1;
-        altePos[1] = -1;
-
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                while(spielLaeuft){
-                    //System.out.println(meinZug);
-                    if(meinZug){
-                        int[] schussKoordinaten = gui.Spielfeld.getPos();
-                        int x = schussKoordinaten[0];
-                        int y = schussKoordinaten[1];
-                        if (x != altePos[0] &&  y !=altePos[1]) {
-                            schussVonMenschAufAI(x, y);
-                            System.out.println("Schie�e auf die AI" + x + " " + y);
-                            altePos[0] = x;
-                            altePos[1] = y;
-                        }
-
-
-                    }else{
-                        do{
-                            shot.aischiesst(boardsize, mygrid);
-                        }while(!meinZug);
-                    }
-
-//            if(spielerSpielfeldDurchlaufen()){
-//                spielLaeuft = false;
-//            }
-//
-//            if(aiSpielfeldDurchlaufen()){
-//                spielLaeuft = false;
-//            }
-                }
-            }
-        });
-
-        //gui.Gui.sieg();
-    }
-
-    /**
-     * Hier wird getestet wer am Zug ist, true f�r Spieler, false f�r AI.
+     * Hier wird getestet wer am Zug ist, true fuer Spieler, false fuer AI.
      * @return boolean-Wert wer am Zug ist
      */
     public static boolean testeObMeinSpielzug(){
@@ -78,47 +22,42 @@ public class SpielLogikAI {
     }
 
     /**
-     * Hier schickt der Spieler einen Schuss auf die AI ab.
+     * Hier schickt der Spieler einen Schuss auf die AI ab. Diese Methode testet auch ob ein Schiff getroffen wurde
+     * und ob ein Spiel bereits zuende ist oder nicht.
      * @param x X-Koordinate vom Schuss
      * @param y Y-Koordinate vom Schuss
-     * @return boolean-Wert ob der Schuss erfolgreich abgeschickt wurde
      */
     public static void schussVonMenschAufAI(int x, int y){
-//        if(testeObMeinSpielzug()){
-            //Teste ob der Schuss irgendein Schiff getroffen hat
-            if (aigrid[x][y].equals(2) || aigrid[x][y].equals(3) || aigrid[x][y].equals(4) ||
-                    aigrid[x][y].equals(5) || aigrid[x][y].equals(6) || aigrid[x][y].equals(7)) {
-                aigrid[x][y] = 21;
-                //gui.Spielfeld.updatefeld(aigrid);
-                Window.updateGrid(mygrid, aigrid);
-                Window.lp.validate();
-                System.out.println("getroffen.");
-        		//CHECK IF WON
-        		if(aiSpielfeldDurchlaufen()) {
-        			System.out.println("YOU WIN");
-        			Window.won.setVisible(true);
-        			Window.menu.setVisible(true);
-        			Window.quit.setVisible(true);
-        		}
-        		if(spielerSpielfeldDurchlaufen()) {
-        			System.out.println("YOU LOST");
-        			Window.lost.setVisible(true);
-        			Window.menu.setVisible(true);
-        			Window.quit.setVisible(true);
-        		}
-                Window.myTurn();
-            }else{
-                aigrid[x][y] = 20;
-                Window.updateGrid(mygrid, aigrid);
-                Window.myT.setVisible(false);
-                Window.lp.validate();
-                System.out.println("nicht getroffen.");
-                Window.aiTurn();
+        //Teste ob der Schuss irgendein Schiff getroffen hat
+        if (aigrid[x][y].equals(2) || aigrid[x][y].equals(3) || aigrid[x][y].equals(4) ||
+                aigrid[x][y].equals(5) || aigrid[x][y].equals(6) || aigrid[x][y].equals(7)) {
+            aigrid[x][y] = 21;
+            //gui.Spielfeld.updatefeld(aigrid);
+            Window.updateGrid(mygrid, aigrid);
+            Window.lp.validate();
+            System.out.println("getroffen.");
+            //CHECK IF WON
+            if(aiSpielfeldDurchlaufen()) {
+                System.out.println("YOU WIN");
+                Window.won.setVisible(true);
+                Window.menu.setVisible(true);
+                Window.quit.setVisible(true);
             }
-//        }else{
-//            System.out.println("ERROR BEIM SCHIESSEN VON MENSCH AUF AI");
-//            
-//        }
+            if(spielerSpielfeldDurchlaufen()) {
+                System.out.println("YOU LOST");
+                Window.lost.setVisible(true);
+                Window.menu.setVisible(true);
+                Window.quit.setVisible(true);
+            }
+            Window.myTurn();
+        }else{
+            aigrid[x][y] = 20;
+            Window.updateGrid(mygrid, aigrid);
+            Window.myT.setVisible(false);
+            Window.lp.validate();
+            System.out.println("nicht getroffen.");
+            Window.aiTurn();
+        }
     }
 
     /**
